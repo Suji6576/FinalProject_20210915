3ba74a1040fc8fdeb9680f5e1cfd1472ef65ff33
안수지 <wwsa2@naver.com>
2021-09-16 12:43:32 +0900

    BaseFragment에서 레트로핏 관련 변수 물려주도록.  => 프래그먼트도, 액티비티처럼 API호출 가능

--- app/src/main/java/com/neppplus/finalproject_20210915/fragments/BaseFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_20210915/fragments/BaseFragment.kt
@@ -1,11 +1,25 @@
 package com.neppplus.finalproject_20210915.fragments
 
 import android.content.Context
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.neppplus.finalproject_20210915.web.ServerAPI
+import com.neppplus.finalproject_20210915.web.ServerAPIService
+import retrofit2.Retrofit
 
 abstract class BaseFragment :Fragment() {
 
     lateinit var mContext: Context
+
+    private lateinit var retrofit: Retrofit
+    lateinit var apiService : ServerAPIService
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mContext = requireContext()
+        retrofit = ServerAPI.getRetrofit(mContext)
+        apiService = retrofit.create(ServerAPIService::class.java)
+    }
 
     abstract fun setUpEvents()
     abstract fun setValues()
