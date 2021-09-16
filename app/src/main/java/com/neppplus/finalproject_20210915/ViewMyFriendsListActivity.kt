92ce45e7e527ffd249b339098c638da78d399633
안수지 <wwsa2@naver.com>
2021-09-16 18:22:15 +0900

    친구요청 리사이클뷰 작업중

--- app/src/main/java/com/neppplus/finalproject_20210915/ViewMyFriendsListActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_20210915/ViewMyFriendsListActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.neppplus.finalproject_20210915.adapters.FriendPagerAdapter
 import com.neppplus.finalproject_20210915.databinding.ActivityViewMyFriendsListBinding
+import com.neppplus.finalproject_20210915.datas.BasicResponse
 
 class ViewMyFriendsListActivity : BaseActivity() {
 
@@ -23,10 +26,15 @@ class ViewMyFriendsListActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        binding.friendsViewPager.addOnAdapterChangeListener(object : TabLayout.ViewPagerOnTabSelectedListener {
+//
+//        })
+
         addBtn.setOnClickListener {
             val myIntent = Intent(mContext, AddFriendActivity::class.java)
             startActivity(myIntent)
         }
+
 
     }
 
--- app/src/main/java/com/neppplus/finalproject_20210915/fragments/RequestedUserListFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_20210915/fragments/RequestedUserListFragment.kt
@@ -6,12 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.neppplus.finalproject_20210915.R
+import com.neppplus.finalproject_20210915.adapters.MyFriendsRecyclerAdapter
 import com.neppplus.finalproject_20210915.databinding.FragmentRequestedUserListBinding
+import com.neppplus.finalproject_20210915.datas.BasicResponse
+import com.neppplus.finalproject_20210915.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RequestedUserListFragment : BaseFragment() {
 
     lateinit var binding:  FragmentRequestedUserListBinding
+
+    val mReqUserList = ArrayList<UserData>()
+    lateinit var mReqUserAdapter: MyFriendsRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +40,45 @@ class RequestedUserListFragment : BaseFragment() {
 
     override fun setUpEvents() {
 
+
+
     }
 
     override fun setValues() {
 
+        mReqUserAdapter = MyFriendsRecyclerAdapter(mContext, mReqUserList)
+        binding.ReqUserRecyclerView.adapter = mReqUserAdapter
+
+        binding.ReqUserRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getReqAddFriendListFromServer()
+    }
+
+    fun getReqAddFriendListFromServer() {
+
+        apiService.postRequestAddFriend(id).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    val basicResponse = response.body()!!
+
+                    mReqUserList.clear()
+                    mReqUserList.addAll(basicResponse.data.friends)
+                    mReqUserAdapter.notifyDataSetChanged()
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 
 
