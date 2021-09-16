2b3ab6ac2174d6f5936ad510ae74154cdefdc50b
안수지 <wwsa2@naver.com>
2021-09-16 12:20:23 +0900

    프래그먼트에 데이터바인딩 처리

--- app/src/main/java/com/neppplus/finalproject_20210915/fragments/MyFriendsListFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_20210915/fragments/MyFriendsListFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.neppplus.finalproject_20210915.R
+import com.neppplus.finalproject_20210915.databinding.FragmentMyFriendsListBinding
 
 class MyFriendsListFragment : Fragment() {
+
+    lateinit var binding: FragmentMyFriendsListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_friends_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_friends_list, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
