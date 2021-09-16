f1ba03dc569ecce151ab6d561719418cf0b8c24d
안수지 <wwsa2@naver.com>
2021-09-16 15:00:21 +0900

    친구목록 실제로 불러내서 => 목록에 뜨게  - 실데이터 반영X  - 프래그먼트도 화면에 붙이지 않은 상태.  - 화면에 아무것도 안나옴

--- app/src/main/java/com/neppplus/finalproject_20210915/fragments/MyFriendsListFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_20210915/fragments/MyFriendsListFragment.kt
@@ -7,11 +7,19 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.neppplus.finalproject_20210915.R
+import com.neppplus.finalproject_20210915.adapters.MyFriendsRecyclerAdapter
 import com.neppplus.finalproject_20210915.databinding.FragmentMyFriendsListBinding
+import com.neppplus.finalproject_20210915.datas.BasicResponse
+import com.neppplus.finalproject_20210915.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyFriendsListFragment : BaseFragment() {
 
     lateinit var binding: FragmentMyFriendsListBinding
+    val mFriendsList = ArrayList<UserData>()
+    lateinit var mFriendAdapter: MyFriendsRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,9 +38,45 @@ class MyFriendsListFragment : BaseFragment() {
 
     override fun setUpEvents() {
 
+
+
     }
 
     override fun setValues() {
 
+        mFriendAdapter = MyFriendsRecyclerAdapter(mContext, mFriendsList)
+        binding.myFriendsRecyclerView.adapter = mFriendAdapter
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        getMyFriendsListFromServer()
+    }
+
+    fun getMyFriendsListFromServer() {
+
+        apiService.getRequestFriendList("my").enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+
+                    val BasicResponse = response.body()!!
+
+                    mFriendsList.clear()
+
+                    mFriendsList.addAll(BasicResponse.data.friends)
+                    mFriendAdapter.notifyDataSetChanged()
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+    }
+
 }
