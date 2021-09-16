09ffae4842b47cab92409b4681f44289406d9c24
안수지 <wwsa2@naver.com>
2021-09-16 15:36:11 +0900

    친구 추가 화면 UI 작업

---
+++ app/src/main/java/com/neppplus/finalproject_20210915/AddFriendActivity.kt
@@ -0,0 +1,19 @@
+package com.neppplus.finalproject_20210915
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFriendActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friend)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
