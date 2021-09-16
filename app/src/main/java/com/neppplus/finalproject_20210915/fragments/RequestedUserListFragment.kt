3d81b196f86c2da0a817d5f2079c47f538277670
안수지 <wwsa2@naver.com>
2021-09-16 12:35:36 +0900

    친구 요청 목록 표시 프래그먼트 추가

---
+++ app/src/main/java/com/neppplus/finalproject_20210915/fragments/RequestedUserListFragment.kt
@@ -0,0 +1,31 @@
+package com.neppplus.finalproject_20210915.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.neppplus.finalproject_20210915.R
+import com.neppplus.finalproject_20210915.databinding.FragmentMyFriendsListBinding
+
+class RequestedUserListFragment : Fragment() {
+
+    lateinit var binding : FragmentMyFriendsListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_requested_user_list, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+
+}
