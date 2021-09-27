8f4f619cb20b94c34160907902122fe4fda18e3a
안수지 <wwsa2@naver.com>
2021-09-27 09:57:43 +0900

    #4 내 장소확인 액티비티 생성

---
+++ app/src/main/java/com/neppplus/finalproject_20210915/ViewMyPlaceMapActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.finalproject_20210915
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMyPlaceMapActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_my_place_map)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
